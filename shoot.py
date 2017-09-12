aff3a430885387b576eb1b448bd3397d0ef11e4f
Matrix <codecolor@outlook.com>
2017-09-12 10:35:08 +0800

    调整查找页面元素的方式

--- shoot.py
+++ shoot.py
@@ -7,6 +7,7 @@
 # @Software: PyCharm
 import os
 import platform
+from functools import partial
 from selenium import webdriver
 from config import current_config
 from selenium.webdriver.support.ui import Select
@@ -15,7 +16,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 __author__ = 'blackmatrix'
 
 
-class QuickBuy:
+class AutoTest:
 
     def __init__(self):
         # 初始化浏览器
@@ -29,32 +30,64 @@ class QuickBuy:
         self.driver = webdriver.Chrome(executable_path=executable_path, chrome_options=options)
         self.driver.implicitly_wait(current_config['TIME_OUT'])
 
-    def find_element_by_id(self, element_id):
-        element = Select(
-            WebDriverWait(
-                driver=self.driver,
-                timeout=current_config['TIME_OUT'],
-                poll_frequency=0.1
-            ).until(lambda x: x.find_element_by_id(element_id)))
+    def wait_find_element_by_id(self, element_id, parent=None):
+        element = WebDriverWait(
+            driver=parent or self.driver,
+            timeout=current_config['TIME_OUT'],
+            poll_frequency=0.1
+        ).until(lambda x: x.find_element_by_id(element_id))
+        self.element_monkey_patch(element)
         return element
 
-    def find_element_by_xpath(self, xpath):
-        element = Select(
-            WebDriverWait(
-                driver=self.driver,
-                timeout=current_config['TIME_OUT'],
-                poll_frequency=0.1
-            ).until(lambda x: x.find_element_by_xpath(xpath)))
+    def wait_find_element_by_xpath(self, xpath, parent=None):
+        element = WebDriverWait(
+            driver=parent or self.driver,
+            timeout=current_config['TIME_OUT'],
+            poll_frequency=0.1
+        ).until(lambda x: x.find_element_by_xpath(xpath))
+        self.element_monkey_patch(element)
         return element
+
+    def wait_find_elements_by_xpath(self, xpath, parent=None):
+        elements = WebDriverWait(
+            driver=parent or self.driver,
+            timeout=current_config['TIME_OUT'],
+            poll_frequency=0.1
+        ).until(lambda x: x.find_elements_by_xpath(xpath))
+        self.elements_monkey_patch(elements)
+        return elements
+
+    def wait_find_element_by_class_name(self, class_name, parent=None):
+        element = WebDriverWait(
+            driver=parent or self.driver,
+            timeout=current_config['TIME_OUT'],
+            poll_frequency=0.1
+        ).until(lambda x: x.find_element_by_class_name(class_name))
+        self.element_monkey_patch(element)
+        return element
+
+    def element_monkey_patch(self, element):
+        element.wait_find_element_by_id = partial(self.wait_find_element_by_id, parent=element)
+        element.wait_find_element_by_xpath = partial(self.wait_find_element_by_xpath, parent=element)
+        element.wait_find_elements_by_xpath = partial(self.wait_find_elements_by_xpath, parent=element)
+        element.wait_find_element_by_class_name = partial(self.wait_find_element_by_class_name, parent=element)
+
+    def elements_monkey_patch(self, elements):
+        try:
+            iter(elements)
+        except ValueError:
+            elements = [elements]
+        finally:
+            for element in elements:
+                self.element_monkey_patch(element)
+
+
+class QuickBuy(AutoTest):
 
     def select_iphone(self, store):
-        # 打开购买页面
-        self.driver.get('http://www.baidu.com')
-        # 选择零售店
-        select_store = self.find_element_by_id('selectStore')
-        select_store.select_by_value(store)
-        # 选择机型
-        select_subfamily = self.find_element_by_id('selectSubfamily')
+        pass
+
+
 
 
 if __name__ == '__main__':
