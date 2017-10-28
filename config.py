e7dc6eb5f7129555f54a9687c97b434076ba951f
Matrix <codecolor@outlook.com>
2017-10-28 12:51:26 +0800

    修改进程数

--- config.py
+++ config.py
@@ -126,7 +126,7 @@ class CommonConfig(BaseConfig):
     # 轮询间隔
     POLL_FREQUENCY = 0.2
     # 进程数，不建议超过CPU核数
-    MULTIPROCESSING = 1
+    MULTIPROCESSING = 2
 
     # GAMMU
     FILECONFIG = os.path.abspath('.gammurc')
