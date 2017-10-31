579f292f168b4716dd289a967733c4f475bf55dd
Matrix <codecolor@outlook.com>
2017-10-31 17:39:36 +0800

    完善日志记录

--- sms/huawei.py
+++ sms/huawei.py
@@ -7,6 +7,7 @@
 # @File : huawei.py
 # @Software: PyCharm
 import gammu
+import logging
 from time import sleep
 from config import current_config
 
@@ -17,9 +18,12 @@ class SMSCenter:
 
     def __init__(self):
         self.state_machine = gammu.StateMachine()
+        logging.info('[信使] 实例化完成')
         self.state_machine.ReadConfig(Filename=current_config['FILECONFIG'])
+        logging.info('[信使] 读取短信设备配置完成')
         # Connect to the phone
         self.state_machine.Init()
+        logging.info('[信使] 初始化完成')
 
     def send_msg(self, targets, content):
         message = {
@@ -28,18 +32,20 @@ class SMSCenter:
             'Number': targets,
             'Coding': 'Unicode_No_Compression'
         }
-        print(message)
+        logging.info('[信使] 开始发送短信：{}'.format(message))
         self.state_machine.SendSMS(message)
+        logging.info('[信使] 发送短信完成')
 
     def get_msg(self):
         location = 1
         while True:
             try:
                 sms = self.state_machine.GetSMS(Folder=1, Location=location)
+                logging.info('[信使] 收取短信完成：{}'.format(sms))
                 if sms:
                     return sms
             except Exception as ex:
-                print(ex)
+                logging.error('[信使] 短信收取失败，准备重试：{}'.format(ex))
                 sleep(0.5)
 
     def del_msgs(self):
@@ -50,19 +56,13 @@ class SMSCenter:
         while True:
             try:
                 self.state_machine.DeleteSMS(Folder=1, Location=localtion)
+                logging.info('[信使] 删除所有短信')
                 localtion += 1
             except Exception as ex:
-                print(ex)
+                logging.error('[信使] 删除短信失败：{}'.format(ex))
                 break
-
-
-
-        # a = self.state_machine.GetSMS(Folder=1, Location=1)
-        # self.state_machine.SMS
-        # self.state_machine.DeleteSMS(Folder=1, Location=1)
-        # b = self.state_machine.GetSMSFolders()
-        # print(a)
 
 
 if __name__ == '__main__':
     pass
+
